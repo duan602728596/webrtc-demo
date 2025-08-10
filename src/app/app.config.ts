f64b7a8477967e322a30c1e323b9292bdf2e13ab
duan602728596 <duanhaochen@126.com>
2025-08-10 14:22:23 +0800

    feat: 升级到angular的v20版本

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { provideExperimentalZonelessChangeDetection, type ApplicationConfig } from '@angular/core';
+import { provideZonelessChangeDetection, type ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
@@ -10,7 +10,7 @@ import { chatroomReducer } from './chatroom/chatroom.reducer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideExperimentalZonelessChangeDetection(),
+    provideZonelessChangeDetection(),
     provideHttpClient(withInterceptorsFromDi()),
     provideAnimations(),
     provideRouter(routes),
